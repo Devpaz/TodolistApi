a81fd6f9a053cf1aad8ac9416de2db53ff8cd469
Paolo Paz <sl.paolo.paz@gmail.com>
2024-11-28 00:12:19 -0500

    feat: add  new Model Task and Add new migrations tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['dni', 'title', 'description', 'due_date', 'status'];
+
 }
--- database/migrations/2024_11_27_162729_create_tasks_table.php
+++ database/migrations/2024_11_27_162729_create_tasks_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('dni');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('due_date');
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
             $table->timestamps();
         });
     }
